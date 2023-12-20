fc63ac3dcffdea4c092f638adba88513c250f833
GonerLCP <antonin.arnold54700@gmail.com>
2023-12-20 15:50:26 +0100

    TestGo

--- Assets/Script/Talk.cs
+++ Assets/Script/Talk.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
+using UnityEngine.UI;
+
 
 public class Talk : MonoBehaviour
 {
     public TMP_Text dialogue;
-    public string textToType;
+    public TMP_Text nomTxtBox;
+    //public string textToType;
     public GameObject IwaiTextBox;
+    public GameObject SpriteBox;
+    public List<string> PhraseaDire = new List<string>();
+    public int phrase;
+    public string nom;
+    public Sprite image;
 
     // Start is called before the first frame update
     void Start()
     {
         IwaiTextBox.SetActive(false);
+        nomTxtBox.text = nom;
+        SpriteBox.GetComponent<Image>().sprite = image;
+        phrase = 0;
     }
 
     // Update is called once per frame
@@ -24,11 +36,25 @@ public class Talk : MonoBehaviour
     public void DeclencherDialogueIwai()
     {
         IwaiTextBox.SetActive(true);
-        StartCoroutine(TaperLeettrePar());
-
+        phrase++;
+        StartCoroutine(TaperLeettrePar(PhraseaDire.ElementAt(0)));
     }
 
-    IEnumerator TaperLeettrePar()
+    public void SuiteDialogue()
+    {
+        if (phrase < PhraseaDire.Count)
+        {
+            phrase++;
+            StartCoroutine(TaperLeettrePar(PhraseaDire.ElementAt(phrase-1)));
+        }
+        else
+        {
+            phrase = 0;
+            IwaiTextBox.SetActive(false);
+        }
+    }
+
+    IEnumerator TaperLeettrePar(string textToType)
     {
         dialogue.text = string.Empty;
 
